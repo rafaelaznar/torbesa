9ab072088a55548bbc36a29f2c3d31ad4e0a2042
Ianechu <ianechu181005@gmail.com>
2025-10-07 23:52:02 +0200

    Modify AUTO_INCREMENT for dog_score table
    Added AUTO_INCREMENT modification for dog_score table.

--- database.sql
+++ database.sql
@@ -90,7 +90,11 @@ ALTER TABLE `users`
 --
 ALTER TABLE `capitals_score`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
+--
+-- AUTO_INCREMENT for table `dog_score`
+--
+ALTER TABLE dog_score
+  MODIFY id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `users`
 --
