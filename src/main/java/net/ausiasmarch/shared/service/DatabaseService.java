f722d1a101b1fae8fa9f8b92f545144270a629b4
Ivan <Minepachin007@gmail.com>
2025-10-08 13:03:40 +0200

    ERRORES arreglados

--- src/main/java/net/ausiasmarch/shared/service/DatabaseService.java
+++ src/main/java/net/ausiasmarch/shared/service/DatabaseService.java
@@ -2,7 +2,7 @@ package net.ausiasmarch.shared.service;
 
 
 public class DatabaseService {
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/torbesa";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/torbesa2";
     public static final String DB_USER = "root";
     public static final String DB_PASS = "tiger";
     public static final int DB_MAX_POOL_SIZE = 10;
