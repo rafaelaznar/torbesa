8af89bd973fd5a6a6d790b7790c3cad1f7f3ae4d
ian <ianechu181005@gmail.com>
2025-10-08 13:41:19 +0200

    Add dog_score table structure and set AUTO_INCREMENT

--- database.sql
+++ database.sql
@@ -160,8 +160,7 @@ ALTER TABLE `trivial_score`
 --
 -- AUTO_INCREMENT for table `dog_score`
 --
-ALTER TABLE `dog_score`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
@@ -747,4 +746,55 @@ ALTER TABLE `wtpokemon_score`
 -- Filtros para la tabla `wtpokemon_score`
 --
 
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: database:3306
+-- Tiempo de generación: 08-10-2025 a las 11:37:37
+-- Versión del servidor: 8.4.6
+-- Versión de PHP: 8.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Base de datos: `torbesa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dog_score`
+--
+
+CREATE TABLE `dog_score` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `score` int NOT NULL,
+  `tries` int NOT NULL,
+  `timestamp` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `dog_score`
+--
+ALTER TABLE `dog_score`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `dog_score`
+--
+ALTER TABLE `dog_score`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
